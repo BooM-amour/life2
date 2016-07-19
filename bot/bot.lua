d25f350b2dd280b3633b01c7cc48feb2642fe803
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-07-19 04:39:44 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {219339258,221176886},
+    sudo_users = {219339258,221176886,221633324},
     admin_users = {},
     disabled_channels = {}
   }
